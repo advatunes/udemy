e044acf0e975afbd3ee923a59099692589860f16
adv <adv@.com>
2020-08-23 16:54:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");g
+
+//const answer = promt("Вам есть 18", "18");
+//console.log(answer);
+
+/* 
+const answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+*/ 
+
+/* const user = "Ivan";
+alert(`Привет, ${user}`);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+*/ 
+
+const isCheked = true,
+      isClose = true;
+
+console.log(isCheked && isClose);
