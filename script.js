14eb98478445ca670653d0736adaafeebb673720
adv <adv@.com>
2020-08-23 18:06:21 +0300

    first practice

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-'use strict';
+const numberOfFilms = +promt('Сколько фильмов?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-//alert('Hello');
+const a = promt('один из последний фильмов?', ''),
+      b = promt('на сколько оцениваете?', ''),
+      c = promt('один из последний фильмов?', ''),
+      d = promt('на сколько оцениваете?', '');
 
-//const result = confirm("Are you here?");g
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-//const answer = promt("Вам есть 18", "18");
-//console.log(answer);
-
-/* 
-const answers = [];
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваше фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
-
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
-*/ 
-
-/* const user = "Ivan";
-alert(`Привет, ${user}`);
-
-console.log(5%2);
-
-console.log(2*4 == 8);
-*/ 
-
-const isCheked = true,
-      isClose = true;
-
-console.log(isCheked && isClose);
+console.log(personalMovieDB);
